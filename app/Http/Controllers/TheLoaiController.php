b83f8d6852700ab88953202256a57ba358f57ff8
Tuan <pokemonred97@gmail.com>
2018-03-27 14:22:02 +0700

    hien thị danh sach the loai

--- app/Http/Controllers/TheLoaiController.php
+++ app/Http/Controllers/TheLoaiController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\TheLoai;
 
 class TheLoaiController extends Controller
 {
     //
     public function getDanhSach()
     {
-    	return view('admin.theloai.danhsach');
+        $theloai = TheLoai::all();  // lấy toàn bộ dữ liệu vào biến thể loại
+    	return view('admin.theloai.danhsach', ['theloai'=>$theloai]);
     }
 
     public function getSua()
--- resources/views/admin/theloai/danhsach.blade.php
+++ resources/views/admin/theloai/danhsach.blade.php
@@ -6,8 +6,8 @@
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-lg-12">
-                        <h1 class="page-header">Category
-                            <small>List</small>
+                        <h1 class="page-header">Thể loại
+                            <small>Danh sách</small>
                         </h1>
                     </div>
                     <!-- /.col-lg-12 -->
@@ -15,30 +15,20 @@
                         <thead>
                             <tr align="center">
                                 <th>ID</th>
-                                <th>Name</th>
-                                <th>Category Parent</th>
-                                <th>Status</th>
+                                <th>Tên</th>
                                 <th>Delete</th>
                                 <th>Edit</th>
                             </tr>
                         </thead>
-                        <tbody>
+                        <tbody> 
+                            @foreach($theloai as $tl) <!--  bên controller -->
                             <tr class="odd gradeX" align="center">
-                                <td>1</td>
-                                <td>Tin Tức</td>
-                                <td>None</td>
-                                <td>Hiện</td>
-                                <td class="center"><i class="fa fa-trash-o  fa-fw"></i><a href="#"> Delete</a></td>
-                                <td class="center"><i class="fa fa-pencil fa-fw"></i> <a href="#">Edit</a></td>
+                                <td>{{$tl -> id}}</td>
+                                <td>{{$tl -> Ten}}</td>
+                                <td class="center"><i class="fa fa-trash-o  fa-fw"></i><a href="admin/theloai/xoa"> Delete</a></td>
+                                <td class="center"><i class="fa fa-pencil fa-fw"></i> <a href="admin/theloai/sua/{{$tl -> id}}">Edit</a></td>
                             </tr>
-                            <tr class="even gradeC" align="center">
-                                <td>2</td>
-                                <td>Bóng Đá</td>
-                                <td>Thể Thao</td>
-                                <td>Ẩn</td>
-                                <td class="center"><i class="fa fa-trash-o  fa-fw"></i><a href="#"> Delete</a></td>
-                                <td class="center"><i class="fa fa-pencil fa-fw"></i> <a href="#">Edit</a></td>
-                            </tr>
+                            @endforeach
                         </tbody>
                     </table>
                 </div>
