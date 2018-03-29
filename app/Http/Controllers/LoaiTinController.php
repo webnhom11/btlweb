26ca95a44f574d1838028ca2e0a2827d610c7f03
Tuan <pokemonred97@gmail.com>
2018-03-29 07:30:49 +0700

    Sua loai tin

--- app/Http/Controllers/LoaiTinController.php
+++ app/Http/Controllers/LoaiTinController.php
@@ -45,11 +45,30 @@ class LoaiTinController extends Controller
 
     public function getSua($id)
     {
-
+    	$theloai = TheLoai::all();
+    	$loaitin = LoaiTin::find($id);
+    	return view('admin.loaitin.sua',['loaitin'=>$loaitin, 'theloai'=>$theloai]);
     }
 
     public function postSua(Request $request, $id){
-
+    	$this->validate($request,
+    		[
+    			'Ten' => 'required|unique:LoaiTin,Ten|min:1|max:100',
+    			'TheLoai' => 'required'
+    		],
+    		[
+    			'Ten.required' => 'Bạn chưa nhập tên loại tin',
+    			'Ten.unique' => 'Loại tin đã tồn tại',
+    			'Ten.min' => 'Tên loại tin phải có độ dài từ 1 đến 100 lí tự',
+    			'Ten.max' => 'Tên loại tin phải có độ dài từ 1 đến 100 lí tự',
+    			'TheLoai.required' => 'Bạn chưa chọn thể loại',
+    		]);
+    	$loaitin = LoaiTin::find($id);
+    	$loaitin->Ten = $request->Ten;
+    	$loaitin->TenKhongDau = changeTitle($request->Ten);
+    	$loaitin->idTheLoai = $request->TheLoai;
+    	$loaitin->save();
+    	return redirect('admin/loaitin/sua/'.$id)->with('thongbao', 'Sửa thành công');
     }
 
     public function getXoa($id){
