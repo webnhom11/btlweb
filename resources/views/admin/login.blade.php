610ba31c349403f04ffb6f39ef93d83d846c2f16
vutien97 <vutien97dhtb@gmail.com>
2018-04-13 15:18:28 +0700

    update

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -8,18 +8,18 @@
     <meta name="author" content="">
 
     <title>Admin - Khoa Phạm</title>
-
+    <base href="{{asset('')}}">
     <!-- Bootstrap Core CSS -->
-    <link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="admin_asset/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- MetisMenu CSS -->
-    <link href="bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
+    <link href="admin_asset/bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link href="dist/css/sb-admin-2.css" rel="stylesheet">
+    <link href="admin_asset/dist/css/sb-admin-2.css" rel="stylesheet">
 
     <!-- Custom Fonts -->
-    <link href="bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="admin_asset/bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
 </head>
 
@@ -33,15 +33,27 @@
                         <h3 class="panel-title">Please Sign In</h3>
                     </div>
                     <div class="panel-body">
-                        <form role="form" action="" method="POST">
+                        @if(count($errors) > 0)
+                            <div class="alert alert-danger">
+                                @foreach($errors->all() as $err)
+                                    {{$err}}<br>
+                                @endforeach
+                            </div>
+                        @endif
+
+                        @if(session('thongbao'))
+                                {{session('thongbao')}}
+                        @endif
+                        <form role="form" action="admin/dangnhap" method="POST">
                             <fieldset>
+                                <input type="hidden" name="_token" value="{{csrf_token()}}"/>
                                 <div class="form-group">
                                     <input class="form-control" placeholder="E-mail" name="email" type="email" autofocus>
                                 </div>
                                 <div class="form-group">
                                     <input class="form-control" placeholder="Password" name="password" type="password" value="">
                                 </div>
-                                <button type="submit" class="btn btn-lg btn-success btn-block">Login</button>
+                                <button type="submit" class="btn btn-lg btn-success btn-block">Đăng nhập</button>
                             </fieldset>
                         </form>
                     </div>
@@ -51,16 +63,16 @@
     </div>
 
     <!-- jQuery -->
-    <script src="bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="admin_asset/bower_components/jquery/dist/jquery.min.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="admin_asset/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 
     <!-- Metis Menu Plugin JavaScript -->
-    <script src="bower_components/metisMenu/dist/metisMenu.min.js"></script>
+    <script src="admin_asset/bower_components/metisMenu/dist/metisMenu.min.js"></script>
 
     <!-- Custom Theme JavaScript -->
-    <script src="dist/js/sb-admin-2.js"></script>
+    <script src="admin_asset/dist/js/sb-admin-2.js"></script>
 
 </body>
 
--- resources/views/admin/user/sua.blade.php
+++ resources/views/admin/user/sua.blade.php
@@ -5,47 +5,64 @@
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-lg-12">
-                        <h1 class="page-header">Category
-                            <small>Edit</small>
+                        <h1 class="page-header">User
+                            <small>{{$user->name}}</small>
                         </h1>
                     </div>
                     <!-- /.col-lg-12 -->
                     <div class="col-lg-7" style="padding-bottom:120px">
-                        <form action="" method="POST">
+                        @if(count($errors) > 0)
+                            <div class="alert alert-danger">
+                                @foreach($errors->all() as $err)
+                                    {{$err}}<br>
+                                @endforeach
+                            </div>
+                        @endif
+
+                        @if(session('thongbao'))
+                            <div class="alert alert-success">
+                                {{session('thongbao')}}
+                            </div>
+                        @endif
+                        <form action="admin/user/sua/{{$user->id}}" method="POST">
+                            <input type="hidden" name="_token" value="{{csrf_token()}}" />
                             <div class="form-group">
-                                <label>Category Parent</label>
-                                <select class="form-control">
-                                    <option value="0">Please Choose Category</option>
-                                    <option value="">Tin Tức</option>
-                                </select>
+                                <label>Họ tên</label>
+                                <input class="form-control" name="name" placeholder="Nhập họ tên người dùng" value="{{$user->name}}" />
                             </div>
                             <div class="form-group">
-                                <label>Category Name</label>
-                                <input class="form-control" name="txtCateName" placeholder="Please Enter Category Name" />
+                                <label>Email</label>
+                                <input type="email" class="form-control" name="email" placeholder="Nhập địa chỉ email" value="{{$user->email}}" readonly="" />
                             </div>
                             <div class="form-group">
-                                <label>Category Order</label>
-                                <input class="form-control" name="txtOrder" placeholder="Please Enter Category Order" />
+                                <input type="checkbox" id="changePassword" name="changePassword">
+                                <label>Đổi mật khẩu</label>
+                                <input type="password" class="form-control password" name="password" placeholder="Nhập mật khẩu" disabled="" />
                             </div>
                             <div class="form-group">
-                                <label>Category Keywords</label>
-                                <input class="form-control" name="txtOrder" placeholder="Please Enter Category Keywords" />
+                                <label>Nhập lại mật khẩu</label>
+                                <input type="password" class="form-control password" name="passwordAgain" placeholder="Nhập lại mật khẩu" disabled="" />
                             </div>
                             <div class="form-group">
-                                <label>Category Description</label>
-                                <textarea class="form-control" rows="3"></textarea>
-                            </div>
-                            <div class="form-group">
-                                <label>Category Status</label>
+                                <label>Quyền người dùng</label>
                                 <label class="radio-inline">
-                                    <input name="rdoStatus" value="1" checked="" type="radio">Visible
+                                    <input name="quyen" value="0" 
+                                    @if($user->quyen == 0)
+                                    {{"checked"}}
+                                    @endif
+                                    type="radio">Thường
                                 </label>
                                 <label class="radio-inline">
-                                    <input name="rdoStatus" value="2" type="radio">Invisible
+                                    <input name="quyen" value="1"
+                                    @if($user->quyen == 1)
+                                    {{"checked"}}
+                                    @endif
+                                     type="radio">Admin
                                 </label>
+                                
                             </div>
-                            <button type="submit" class="btn btn-default">Category Edit</button>
-                            <button type="reset" class="btn btn-default">Reset</button>
+                            <button type="submit" class="btn btn-default">Sửa</button>
+                            <button type="reset" class="btn btn-default">Làm mới</button>
                         <form>
                     </div>
                 </div>
@@ -53,4 +70,20 @@
             </div>
             <!-- /.container-fluid -->
         </div>
+@endsection
+
+@section('script')
+    <script>
+        $(document).ready(function(){
+            $("#changePassword").change(function(){
+                if ($(this).is(":checked")) {
+                    $(".password").removeAttr('disabled');
+                }
+                else
+                {
+                    $(".password").attr('disabled','');
+                }
+            });
+        });
+    </script>
 @endsection
